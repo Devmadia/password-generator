105de61de916f53508e5e47fb14ca08d8f4aa80b
Devmadia <agathaphan@icloud.com>
2020-06-12 18:54:37 -0500

    updated script for variables the generated password will pull from

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
 // Assignment code here
 
+// variable character types
+var upperCase = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var availNums = "0123456789";
+var availSpecs = "~!"@#$%^&*'`?()_-=+,/[\]{|}<>:;";
+var minPassLength = 8; // sets minimum
+var maxPassLength = 128; // sets maximum
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+  // input for password length
+
+  // input for password character types
+
 
 // Write password to the #password input
 function writePassword() {
