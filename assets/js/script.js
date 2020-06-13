504685ba06d61474b03458dbbe55add829fd17df
Devmadia <agathaphan@icloud.com>
2020-06-12 19:22:05 -0500

    fixed bracket issue, added debugger, commented out parseInt for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,25 @@
 var upperCase = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var availNums = "0123456789";
-var availSpecs = "~!"@#$%^&*'`?()_-=+,/[\]{|}<>:;";
+var availSpecs = "~!@#$%^&*'`?()_-=+,/[\]{|}<>:;";
 var minPassLength = 8; // sets minimum
 var maxPassLength = 128; // sets maximum
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-  var password = ""; // variable defines password label which will generate later on based on following variables if selected by user
-  
+
+debugger;
+var generatePassword = function() {
+  // variable defines password label which will generate later on based on following variables if selected by user
+  var password = "";
   var passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.")
-  passLength = parseInt(passLength, 8); // input for password length
+  //passLength = parseInt(passLength, 8); // input for password length
 
   while (passLength < 8 || passLength > 128) {
     passLength = window.prompt("Length MUST be 8 - 128 characters. How many characters do you wish your password to be?")
-  } // ensures user enters a password length 
+  }
+ } // ensures user enters a password length 
   // input for password character types
   // make sure user selects character types
   // loop to include specific variables user requested into password
