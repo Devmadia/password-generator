8793e563a3a492b77c0ee535e5830eb0bed5d6f4
Devmadia <agathaphan@icloud.com>
2020-06-12 21:44:00 -0500

    added feature to let user cancel generator options

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,28 +16,31 @@ var generatePassword = function() {
   // variable defines password label which will generate later on based on following variables if selected by user
   var password = "";
 
-
-  // Do while
+  // Do while loop
   //var passLength;
   //do {
   //  passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
-  //} while (isAlpha(passLength));
+  //} while (isNaN(passLength));
 
 
   // While
   var passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
+  if (passLength === null) {
+    return;
+  }
   while (isNaN(passLength)) {
     passLength = window.prompt("Please choose a password length of 8 - 128 characters.");
   }
 
   //passLength = parseInt(passLength, 10); // input for password length
 
+  // ensures user enters a password length 
   while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     passLength = window.prompt("Length MUST be 8 - 128 characters. How many characters do you wish your password to be?")
   }
- } 
+} 
  
- // ensures user enters a password length 
+ 
   // input for password character types
   // make sure user selects character types
   // loop to include specific variables user requested into password
