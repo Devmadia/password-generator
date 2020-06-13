e519c22f38444c323a3faf90db8b84d66b6f0da0
Devmadia <agathaphan@icloud.com>
2020-06-12 22:01:18 -0500

    introducting window.confirm options for character types

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var generateBtn = document.querySelector("#generate");
 debugger;
 var generatePassword = function() {
   // variable defines password label which will generate later on based on following variables if selected by user
-  var password = "";
+  //var password = "";
 
   // Do while loop
   //var passLength;
@@ -28,6 +28,7 @@ var generatePassword = function() {
   if (passLength === null) {
     return;
   }
+  
   while (isNaN(passLength)) {
     passLength = window.prompt("Please choose a password length of 8 - 128 characters.");
   }
@@ -37,6 +38,21 @@ var generatePassword = function() {
   // ensures user enters a password length 
   while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     passLength = window.prompt("Length MUST be 8 - 128 characters. How many characters do you wish your password to be?")
+  }
+
+  if (passLength >= 8 && passLength <= 128) {
+    var upperCase = window.confirm("Do you wish to include CAPITAL letters?");
+    var lowerCase = window.confirm("Do you wish to include LOWER CASE letters?");
+    var availNums = window.confirm("Do you wish to include numerical characters?");
+    var availSpecs = window.confirm("Do you wish to include SPECIAL characters?");
+
+    if (!upperCase && !lowerCase && !availNums && !availSpecs) {
+      window.prompt("You MUST choose at least one character type.");
+      upperCase = window.confirm("Do you wish to include CAPITAL letters?");
+      lowerCase = window.confirm("Do you wish to include LOWER CASE letters?");
+      availNums = window.confirm("Do you wish to include numerical characters?");
+      availSpecs = window.confirm("Do you wish to include SPECIAL characters?");
+    }
   }
 } 
  
