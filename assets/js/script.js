d5c58fcff7234838065657e4f87a57f219558a43
Devmadia <agathaphan@icloud.com>
2020-06-12 19:14:14 -0500

    added variable for password to be stored under, window prompt for user input on password length, response if input is inappropriate

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,11 @@ var maxPassLength = 128; // sets maximum
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
   var password = ""; // variable defines password label which will generate later on based on following variables if selected by user
-  // input for password length
+  var passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.")
+  passLength = parseInt(passLength, 8); // input for password length
+  while (passLength < || passLength > 128) {
+    passLength = prompt("Length MUST be 8 - 128 characters. How many characters do you wish your password to be?")
+  }
   // input for password character types
   // make sure user selects character types
   // loop to include specific variables user requested into password
