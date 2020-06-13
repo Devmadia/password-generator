6dc50886813365b3f912b907172e13211eca038d
Devmadia <agathaphan@icloud.com>
2020-06-12 21:08:51 -0500

    commented out do/while loop, introduced while instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,29 @@ debugger;
 var generatePassword = function() {
   // variable defines password label which will generate later on based on following variables if selected by user
   var password = "";
-  var passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.")
-  //passLength = parseInt(passLength, 8); // input for password length
+
+
+  // Do while
+  //var passLength;
+  //do {
+  //  passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
+  //} while (isAlpha(passLength));
+
+
+  // While
+  var passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
+  while (isNaN(passLength)) {
+    passLength = window.prompt("ERROR: How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
+  }
+
+  passLength = parseInt(passLength, 10); // input for password length
 
   while (passLength < 8 || passLength > 128) {
     passLength = window.prompt("Length MUST be 8 - 128 characters. How many characters do you wish your password to be?")
   }
- } // ensures user enters a password length 
+ } 
+ 
+ // ensures user enters a password length 
   // input for password character types
   // make sure user selects character types
   // loop to include specific variables user requested into password
