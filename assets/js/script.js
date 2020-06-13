64668e422e9a5994a6a12cc533628ef4816436fe
Devmadia <agathaphan@icloud.com>
2020-06-12 22:38:00 -0500

    fixing simple mistakes with typos and got while loop warning working again

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,15 @@ var maxPassLength = 128; // sets maximum
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-debugger;
 var generatePassword = function() {
   // variable defines password label which will generate later on based on following variables if selected by user
-  //var password = "";
+  var password = "";
 
-  // Do while loop
-  //var passLength;
-  //do {
-  //  passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
-  //} while (isNaN(passLength));
-
+  // Do while loop option
+  /*var passLength;
+  do {
+    passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
+  } while (isNaN(passLength));*/
 
   // While
   var passLength = window.prompt("How long do you wish your generated password to be? Please choose a length of 8 - 128 characters.");
@@ -33,7 +31,7 @@ var generatePassword = function() {
     passLength = window.prompt("Please choose a password length of 8 - 128 characters.");
   }
 
-  //passLength = parseInt(passLength, 10); // input for password length
+  passLength = parseInt(passLength, 10); // input for password length
 
   // ensures user enters a password length 
   while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
@@ -42,25 +40,52 @@ var generatePassword = function() {
 
   // input for password character types
   if (passLength >= 8 && passLength <= 128) {
-    var upperCase = window.confirm("Do you wish to include CAPITAL letters?");
-    var lowerCase = window.confirm("Do you wish to include LOWER CASE letters?");
-    var availNums = window.confirm("Do you wish to include numerical characters?");
-    var availSpecs = window.confirm("Do you wish to include SPECIAL characters?");
+    var upperCaseQ = window.confirm("Do you wish to include CAPITAL letters?");
+    var lowerCaseQ = window.confirm("Do you wish to include LOWER CASE letters?");
+    var availNumsQ = window.confirm("Do you wish to include numerical characters?");
+    var availSpecsQ = window.confirm("Do you wish to include SPECIAL characters?");
+    
     // forces user to pick at least one character type to continue
-    while (!upperCase && !lowerCase && !availNums && !availSpecs) {
+    while (!upperCaseQ && !lowerCaseQ && !availNumsQ && !availSpecsQ) {
+      // make sure user selects character types
       window.confirm("WARNING: You must choose at least one character type to continue.");
-      upperCase = window.confirm("Do you wish to include CAPITAL letters?");
-      lowerCase = window.confirm("Do you wish to include LOWER CASE letters?");
-      availNums = window.confirm("Do you wish to include numerical characters?");
-      availSpecs = window.confirm("Do you wish to include SPECIAL characters?");
+      upperCaseQ = window.confirm("Do you wish to include CAPITAL letters?");
+      lowerCaseQ = window.confirm("Do you wish to include LOWER CASE letters?");
+      availNumsQ = window.confirm("Do you wish to include numerical characters?");
+      availSpecsQ = window.confirm("Do you wish to include SPECIAL characters?");
     }
+
+    // loop to include specific variables user requested into password
+    var createPassword = '';
+
+    if (upperCaseQ) {
+      createPassword += upperCase;
+    }
+
+    if (lowerCaseQ) {
+      createPassword += lowerCase;
+    }
+
+    if (availNumsQ) {
+      createPassword += availNums;
+    }
+
+    if (availSpecsQ) {
+      createPassword += availSpecs;
+    }
+
+    for (i = 0; i < passLength; i++) {
+      password += createPassword[Math.floor(Math.random() * createPassword.length)];
+    }
+    debugger;
+  } else {
+    window.alert("Please enter 8 - 128 for character length.");
+    generatePassword();
   }
+
+  return password;
 } 
- 
- 
-  
-  // make sure user selects character types
-  // loop to include specific variables user requested into password
+   
 
 // Write password to the #password input
 function writePassword() {
